aa54c0526398ceaf04214b7eed06f3ff495d4de7
Явор Налбантов <javor.nalbantov@gmail.com>
2026-03-29 19:39:35 +0300

    създаване на тестове

---
+++ ProjectTest/NUnitTestItem.cs
@@ -0,0 +1,36 @@
+﻿namespace WebApplication6;
+
+using GCommon.Data;
+using GCommon.Services;
+using Microsoft.EntityFrameworkCore;
+using NUnit;
+using NUnit.Framework;
+
+public class NUnitTestItem
+{
+    private MeisterContext _context = null;
+    //**********************************************************************************
+    [SetUp]
+    public void SetUp()
+    {
+        // Define a unique name for the in-memory database per test run
+        var options = new DbContextOptionsBuilder<MeisterContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new MeisterContext(options);
+    }
+    //**********************************************************************************
+    [TearDown]
+    public void TearDown()
+    {
+        _context.Dispose();
+    }
+    //**********************************************************************************
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    //**********************************************************************************
+}
