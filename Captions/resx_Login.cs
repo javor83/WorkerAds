21696cfbf0873da2d536f5dc45b1c987fb3c7893
Явор Налбантов <javor.nalbantov@gmail.com>
2026-04-04 11:18:34 +0300

    export

--- Captions/resx_Login.cs
+++ Captions/resx_Login.cs
@@ -5,9 +5,11 @@
         public const string RememberMe = "Запомни ме";
         public const string RegisterNew = "Нова регистрация";
         public const string RequestLogin = "Вход";
+        public const string LoginToSystem = "Вход в системата";
 
         public const string Email = "Поща";
         public const string Password = "Парола";
+
 
 
 
